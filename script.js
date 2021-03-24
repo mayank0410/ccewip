cb29dfecef35de7e74f7b1c7c9bf2f0b16ab1321
Mayank Singh <65878632+mayank0410@users.noreply.github.com>
2021-03-24 19:18:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function menuToggle() {
+    const toggleMenu = document.querySelector('.toggle');
+    const section = document.querySelector('section');
+    toggleMenu.classList.toggle('active');
+    section.classList.toggle('active');
+}
